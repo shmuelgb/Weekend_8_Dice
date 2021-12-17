7861108309726721a3e11f661860ce05317a6933
shmulgb <93119163+shmuelgb@users.noreply.github.com>
2021-12-17 11:19:39 +0200

    Update Dice.js

--- src/components/Dice.js
+++ src/components/Dice.js
@@ -2,14 +2,6 @@ import React from "react";
 import "./Dice.css";
 
 export default function Dice(props) {
-  const dices = {
-    1: "./images/dice-1.png",
-    2: "./images/dice-2.png",
-    3: "./images/dice-3.png",
-    4: "./images/dice-4.png",
-    5: "./images/dice-5.png",
-    6: "./images/dice-6.png",
-  };
   const createDice = () => {
     return (
       <div className="dice-container">
